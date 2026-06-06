6583fd68f4996dca8f96714e1ce356ec9c91c768
black <losidkl@gmail.com>
2026-06-06 17:42:11 +0800

    fix: remove opacity from Pull/Push toolbar buttons

--- webview/src/commit/components/Toolbar.tsx
+++ webview/src/commit/components/Toolbar.tsx
@@ -86,6 +86,7 @@ export function Toolbar({
         <button
           type="button"
           className="commit-toolbar-btn"
+          style={{ opacity: 1 }}
           onClick={() => bridge.request("pullBranch", {})}
         >
           <PullIcon />
@@ -95,6 +96,7 @@ export function Toolbar({
         <button
           type="button"
           className="commit-toolbar-btn"
+          style={{ opacity: 1 }}
           onClick={() => bridge.request("openPushPanel")}
         >
           <PushIcon />
